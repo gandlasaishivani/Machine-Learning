1135651c555bb19d1aa3c2a631cf6efb4c4b09e1
gandlasaishivani <154616791+gandlasaishivani@users.noreply.github.com>
2024-01-27 21:53:49 +0530

    Create loops_strings

---
+++ loops_strings.py
@@ -0,0 +1,75 @@
+age = 18
+if  age>18 or age==18:
+    print("You can vote")
+else :
+    print("You cannot vote")
+  # You can vote
+  for i in range(0,10):
+    print(i)
+    0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+for i in range(0,10,2):
+    print(i)
+a = [4,5,6,7,8,9]
+for i in a :
+    print(i)
+#   0
+# 2
+# 4
+# 6
+# 8
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+for i in range(0,5):
+    for j in range(0,5):
+        print(i+j)
+      0
+# 1
+# 2
+# 3
+# 4
+# 1
+# 2
+# 3
+# 4
+# 5
+# 2
+# 3
+# 4
+# 5
+# 6
+# 3
+# 4
+# 5
+# 6
+# 7
+# 4
+# 5
+# 6
+# 7
+# 8
+a= "   python life  "
+print(a.startswith("python "))
+print(a.replace("life","programming"))
+print(a.count('l'))
+print(a.strip())
+print(a.lstrip())
+print(a.rstrip())
+# False
+#    python programming  
+# 1
+# python life
+# python life  
+#    python life
